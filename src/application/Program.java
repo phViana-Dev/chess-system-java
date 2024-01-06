75e73e28bcee22c543fde1a7cf6965b24592492a
Pedro Viana <dev.pedroviana@gmail.com>
2024-01-06 17:31:59 -0300

    Position created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args){
-        System.out.println("Projeto criado");
+
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
  
 }
