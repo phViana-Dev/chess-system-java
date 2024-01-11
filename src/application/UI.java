2c346e56cb3cac5a8c4dd032afc6cb74d7f0ff12
Pedro Viana <dev.pedroviana@gmail.com>
2024-01-11 16:58:50 -0300

    Handling captured pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -52,12 +52,11 @@ public class UI {
         }
     }
 
-    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured){
         printBoard(chessMatch.getPieces());
         System.out.println();
         printCapturedPieces(captured);
-        System.out.println();
-        System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Turn " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
     }
 
@@ -101,7 +100,7 @@ public class UI {
         System.out.print(" ");
     }
 
-    private static void printCapturedPieces(List<ChessPiece> captured) {
+    private static void printCapturedPieces(List<ChessPiece> captured){
         List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
         List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
         System.out.println("Captured pieces:");
